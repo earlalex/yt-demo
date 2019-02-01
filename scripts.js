9cd945db5c4c046acb158cc27a7a77bb31a0bfe4
Earl Alexander <e.alex@Earls-MacBook-Pro.local>
2019-02-01 06:08:59 -0700

    Changed JS consts into one const object.

--- scripts.js
+++ scripts.js
@@ -1,13 +1,19 @@
-const client         = '393661548459-e1as6ms1k3bkgitfak6u2t17adg92rvf.apps.googleusercontent.com';
-const endpoint       = 'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest';
-const scope          = 'https://www.googleapis.com/auth/youtube.readonly';
-const loginBtn       = document.getElementById('login');
-const logoutBtn      = document.getElementById('logout');
-const loginArea      = document.getElementById('login-area');
-const logoutArea     = document.getElementById('logout-area');
-const form           = document.getElementById('search');
-const formInput      = document.getElementById('form-input');
-const videos         = document.getElementById('videos');
-const noResults      = document.getElementById('no-results');
-const showResults    = document.getElementById('show-results');
-const defaultChannel = 'techguyweb';
+const settings = { 
+	client         : '393661548459-e1as6ms1k3bkgitfak6u2t17adg92rvf.apps.googleusercontent.com',
+	endpoint       : 'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest',
+	scope          : 'https://www.googleapis.com/auth/youtube.readonly',
+	loginBtn       : document.getElementById('login'),
+	logoutBtn      : document.getElementById('logout'),
+	loginArea      : document.getElementById('login-area'),
+	logoutArea     : document.getElementById('logout-area'),
+	form           : document.getElementById('search'),
+	formInput      : document.getElementById('form-input'),
+	videos         : document.getElementById('videos'),
+	noResults      : document.getElementById('no-results'),
+	showResults    : document.getElementById('show-results'),
+	defaultChannel : 'techguyweb'
+};
+
+class YTDemo {
+
+}
