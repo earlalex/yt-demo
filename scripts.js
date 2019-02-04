babd4a92cad2a98d04b4e659e3b62889e2dae6fc
Earl Alexander <e.alex@Earls-MacBook-Pro.local>
2019-02-04 10:24:13 -0700

    Formatting Liked Videos Playlist

--- scripts.js
+++ scripts.js
@@ -74,13 +74,15 @@ function initClient() {
 
 					searchResults.forEach( item => {
 
-						// videoResults.search += `
-						// 	<div class="video-embed">
-						// 		<iframe width="100%" height="auto" src="https://www.youtube.com/embed/${item.snippet.resourceId.videoId}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-						// 	</div>
-						// `;
-				    console.log(item);
+						videoResults.search += `
+							<div class="video-embed">
+								<iframe width="100%" height="auto" src="https://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+							</div>
+						`;
+
 					});
+
+					settings.videos.innerHTML = videoResults.search;
 				});
 			} else {
 
