2eec79fde33e13475e18ab2957f11b23cc024ced
Earl Alexander <e.alex@Earls-MacBook-Pro.local>
2019-02-03 19:18:09 -0700

    Formatting Liked Videos Playlist

--- scripts.js
+++ scripts.js
@@ -97,7 +97,7 @@ function setSigninStatus( isSignedIn ) {
 
 			    		for ( var i = likedPlaylist.length - 1; i >= 0; i-- ) {
 
-			    			likedvideos.i = {
+			    			likedvideos[i] = {
 			    				'channel'     : likedPlaylist[i].snippet.channelTitle,
 			    				'channelId'   : likedPlaylist[i].snippet.channelId,
 			    				'title'       : likedPlaylist[i].snippet.title,
